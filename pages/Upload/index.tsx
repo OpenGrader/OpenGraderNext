318888e41fcf0f5a96babcc26c01471c9064a9e4
ekobedevon <e.kobedevon@gmail.com>
2023-02-14 00:58:27 -0600

    Upload (#16)
    * working on file upload.
    
    * add upload component to send to supabase

---
+++ pages/Upload/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import AssignmentUpload from "../../Components/AssignmentUpload"
+
+const Upload: NextPage = () => {
+
+    return(
+        <>
+        <AssignmentUpload/>
+        </>
+    )
+};
+
+
+export default Upload;
