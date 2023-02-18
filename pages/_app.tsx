425f578f7cfef55d42a3613c92003a2874840e95
Jackson Welsh <me@jacksonwel.sh>
2023-02-18 16:16:30 -0600

    use proper imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import { Head } from "next/document";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
