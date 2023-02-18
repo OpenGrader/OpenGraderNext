e3ae8507f8a446c7fd727b9f2c38b4be980baeab
Jackson Welsh <me@jacksonwel.sh>
2023-02-18 16:09:34 -0600

    move title to _app per nextJS best-practice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,21 @@ import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+import { Head } from "next/document";
 
 function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
+    <>
+      <Head>
+        <title>OpenGrader</title>
+      </Head>
+
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
     <Html lang="en">
-      <Head>
-        <title>OpenGrader</title>
-      </Head>
       <body>
         <Main />
         <NextScript />
