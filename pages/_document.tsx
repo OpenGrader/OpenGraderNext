5c88d3dad548b58f95a2d57393713b4813864087
Jackson Welsh <me@jacksonwel.sh>
2023-02-18 18:17:23 -0600

    move title to _app per nextJS best-practice (#17)
    * move title to _app per nextJS best-practice
    
    * use proper imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
     <Html lang="en">
-      <Head>
-        <title>OpenGrader</title>
-      </Head>
       <body>
+        <Head />
         <Main />
         <NextScript />
       </body>
