eeff81f018627f67d65f37b255be2ae684322961
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2023-03-03 18:17:14 -0600

    created CodeBrowser component

--- Components/CodeBrowser.tsx
+++ Components/CodeBrowser.tsx
@@ -1,3 +1,13 @@
-export const CodeBrowser = () => {
+import Prism from 'prismjs' //used for syntax highlighting
 
+interface CodeBrowserProps {
+    language: string;
+    code: string;
 }
+
+const CodeBrowser: React.FC<CodeBrowserProps> = ({language, code}) => {
+    const html = Prism.highlight(code, Prism.languages[language], language);
+    return <pre dangerouslySetInnerHTML={{__html: html}} />;
+}
+
+export default CodeBrowser;
