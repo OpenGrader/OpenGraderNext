168cec5f91331f0781b1fd44a5728cbf38918e37
e.kobedevon <e.kobedevon@gmail.com>
2023-03-07 00:02:38 -0600

    attempt at using form data to pass assighment, keeping for future use

--- pages/api/createAssignment.ts
+++ pages/api/createAssignment.ts
@@ -1,23 +1,40 @@
 import { NextApiHandler } from "next";
 import { supabaseAdmin } from "util/supabaseClient";
+import { nanoid } from "nanoid";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 export const handler: NextApiHandler = async (req, res) => {
   const body = req.body;
-  console.log("HERE")
-  console.log({ body });
-  
 
+  const upload = async (filePath: string, file: File | undefined) => {
+    await supabaseAdmin.storage
+      .from("spec-storage")
+      .upload(filePath, file || "")
+      .catch((error) => console.log(error));
+  };
+
+  console.log("HERE");
+  console.log({ body });
+
+  const inputID = nanoid();
+  const outputID = nanoid();
+  const inputPath = `${body.courseID}/${inputID}.${body.inputFile?.filename.split(".").pop()}`;
+  const outputPath = `${body.courseID}/${outputID}.${body.outputFile?.filename.split(".").pop()}`;
+
+  upload(inputPath, body.inputFile);
+  upload(outputPath, body.outputFile);
+  console.log(body.courseID)
   const { data: assignment, error } = await supabaseAdmin
     .from("assignment")
     .insert({
-      section: body.section,
+      section: body.courseID,
       title: body.title,
-      description: body.description,
+      description: body.desc,
       is_open: true,
       is_late: false,
-      input_file: body["input-def"],
-      output_file: body["output-def"],
-      language: String(body.language).toLowerCase(),
+      input_file: inputPath,
+      output_file: outputPath,
+      language: String(body.lang).toLowerCase(),
     })
     .select();
 
@@ -28,7 +45,7 @@ export const handler: NextApiHandler = async (req, res) => {
   const assignmentId = assignment?.[0]?.id;
 
   if (assignmentId) {
-    return res.status(200).redirect(`/course/${body.section}/assignment/${assignmentId}`);
+    return res.status(200).json(`/course/${body.section}/assignment/${assignmentId}`);
   }
 
   return res.status(404).json(`"An unknown error has occurred."`);
