a2a44677f9db80dbb0f99c6cf82866fff95db55f
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2023-03-06 18:19:02 -0600

    pad right just a bit

--- Components/CodeBrowser.tsx
+++ Components/CodeBrowser.tsx
@@ -1,5 +1,5 @@
 import Prism from "prismjs"; //used for syntax highlighting
-import "prismjs/components/prism-python"; //supports syntax highlighting for all languages
+import "prismjs/components/prism-python"; //supports syntax highlighting for python only
 import "prismjs/themes/prism-okaidia.css"; //stylizes syntax highlighting
 
 interface CodeBrowserProps {
@@ -14,7 +14,7 @@ const CodeBrowser: React.FC<CodeBrowserProps> = ({ language, code }) => {
     return <div>Value is undefined or null</div>
   }
   const html = Prism.highlight(code, Prism.languages[language], language);
-  return <pre dangerouslySetInnerHTML={{ __html: html }} />;
+  return <pre className="leading-6" style={{ fontFamily: "Jetbrains Mono, monospace", fontSize: "14px"}} dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 export default CodeBrowser;
