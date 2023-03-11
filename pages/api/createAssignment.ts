80e4e3606fb6584e5ef039c5d6dedf2c229cf394
ekobedevon <e.kobedevon@gmail.com>
2023-03-10 23:37:47 -0600

    Assignment upload (#31)
    * move upload box to its own component
    
    * move dynamic assignment route into own folder
    
    * Create student upload page with redirct to course after upload
    
    * implement student upload table to keep track of all student submissions
    
    * Fixed build error
    
    * set file type to zip only and remove unnecsary page
    
    * Add button to link to assignment upload
    
    * deleted old file
    
    * function changed to arrow function for consitency
    
    * added type to types.ts
    
    * changed button styling and updated upload box text to reflect capabilities
    
    * reduced font size for button on submit
    
    * implement use of redux store for user data
    
    * add .zip extension to file path to make them valid
    
    * add .zip extension to file path to make them valid but now gets extension from file name
    
    * added redirect on null assignment object, added conditional rendering to only show upload on proper assignment
    
    * Removed need for file name user state
    
    * added base file upload for input and output files
    
    * case language to string to allow for toLowerCase with no errors
    
    * attempt at using form data to pass assighment, keeping for future use
    
    * add record creator for assignments
    
    ---------
    
    Co-authored-by: Forehand, Dayton <daytonforehand@my.unt.edu>

--- pages/api/createAssignment.ts
+++ pages/api/createAssignment.ts
@@ -2,10 +2,19 @@ import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NextApiHandler } from "next";
 import { getCurrentUser } from "util/misc";
 import { supabaseAdmin } from "util/supabaseClient";
-
+import { nanoid } from "nanoid";
 export const handler: NextApiHandler = async (req, res) => {
   const body = req.body;
 
+  const upload = async (filePath: string, file: File | undefined) => {
+    await supabaseAdmin.storage
+      .from("spec-storage")
+      .upload(filePath, file || "")
+      .catch((error) => console.log(error));
+  };
+
+  console.log("HERE");
+  console.log({ body });
   const userSbase = createServerSupabaseClient({ req, res });
   const user = await getCurrentUser(userSbase);
 
@@ -19,17 +28,25 @@ export const handler: NextApiHandler = async (req, res) => {
     return res.status(403).json(`"Not authorized to create an assignment in section ${body.section}"`);
   }
 
+  const inputID = nanoid();
+  const outputID = nanoid();
+  const inputPath = `${body.courseID}/${inputID}.${body.inputFile?.filename.split(".").pop()}`;
+  const outputPath = `${body.courseID}/${outputID}.${body.outputFile?.filename.split(".").pop()}`;
+
+  upload(inputPath, body.inputFile);
+  upload(outputPath, body.outputFile);
+  console.log(body.courseID)
   const { data: assignment, error } = await supabaseAdmin
     .from("assignment")
     .insert({
-      section: body.section,
+      section: body.courseID,
       title: body.title,
-      description: body.description,
+      description: body.desc,
       is_open: true,
       is_late: false,
-      input_file: body["input-def"],
-      output_file: body["output-def"],
-      language: body.language.toLowerCase(),
+      input_file: inputPath,
+      output_file: outputPath,
+      language: String(body.lang).toLowerCase(),
     })
     .select();
 
@@ -40,7 +57,7 @@ export const handler: NextApiHandler = async (req, res) => {
   const assignmentId = assignment?.[0]?.id;
 
   if (assignmentId) {
-    return res.status(200).redirect(`/course/${body.section}/assignment/${assignmentId}`);
+    return res.status(200).json(`/course/${body.section}/assignment/${assignmentId}`);
   }
 
   return res.status(404).json(`"An unknown error has occurred."`);
