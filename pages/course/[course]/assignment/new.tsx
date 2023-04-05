90c1440e27fa435a50fb70f747042cc8513c47b4
e.kobedevon <e.kobedevon@gmail.com>
2023-04-05 18:37:53 -0500

    add reducer for handling file types and counter

--- pages/course/[course]/assignment/new.tsx
+++ pages/course/[course]/assignment/new.tsx
@@ -2,7 +2,7 @@ import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext, NextPage } from "next";
 import { getCurrentUser, queryParamToNumber } from "util/misc";
 import withProtected from "util/withProtected";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { nanoid } from "nanoid";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
@@ -18,10 +18,8 @@ interface CreateAssignmentProps {
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) =>
   withProtected(ctx, async (ctx) => {
     const courseId = queryParamToNumber(ctx.query?.course);
-
     const supabase = createServerSupabaseClient(ctx);
     const user = await getCurrentUser(supabase);
-
     const { data: currentUserMembership } = await supabase
       .from("membership")
       .select("role")
@@ -45,7 +43,34 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) =>
     };
   });
 
+interface fileState {
+  fileStore: (File | undefined)[];
+}
+type Action = { type: "increment" } | { type: "decrement" };
 
+function fileReducer(state: fileState, action: Action): fileState {
+  switch (action.type) {
+    case "increment":
+      return { fileStore: state.fileStore.concat([undefined, undefined]) };
+    case "decrement":
+      if (state.fileStore.length <= 2) return { fileStore: state.fileStore }; // has to be atleast one case
+      else return { fileStore: state.fileStore.slice(0, -2) };
+    default:
+      throw new Error();
+  }
+}
+
+function countReducer(state: fileState, action: Action): fileState {
+  switch (action.type) {
+    case "increment":
+      return { fileStore: state.fileStore.concat([undefined, undefined]) };
+    case "decrement":
+      if (state.fileStore.length <= 2) return { fileStore: state.fileStore }; // has to be atleast one case
+      else return { fileStore: state.fileStore.slice(0, -2) };
+    default:
+      throw new Error();
+  }
+}
 
 const FileBox = ({ title, file }: { title: string; file?: File }) => {
   return (
@@ -53,7 +78,7 @@ const FileBox = ({ title, file }: { title: string; file?: File }) => {
       <h1 className="text-sm font-medium text-gray-300 pb-2">{title}</h1>
       <label
         htmlFor="input-def"
-        className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-800/25 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-700/25 transition">
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg aria-hidden="true" fill="none" stroke="currentColor" className="w-12 h-12 mx-auto text-gray-600">
             <path
@@ -67,11 +92,11 @@ const FileBox = ({ title, file }: { title: string; file?: File }) => {
               <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                 <span className="font-semibold">Click to upload</span>
               </p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">.zip (max. 50MB)</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">.txt (max. 50MB)</p>
             </>
           ) : (
             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              Currently selected: <span className="font-semibold">{File?.name}</span>
+              Currently selected: <span className="font-semibold">{file?.name}</span>
             </p>
           )}
         </div>
@@ -94,15 +119,34 @@ const CreateAssignment: NextPage<CreateAssignmentProps> = ({ courseId }) => {
   const [title, setTitle] = useState<string>();
   const [lang, setLang] = useState<string>("c/c++");
   const [desc, setDesc] = useState<string>();
+  const [state, dispatch] = useReducer(fileReducer, { fileStore: [undefined, undefined] });
+  const [count, setCount] = useReducer((state: number, action: string) => {
+    switch (action) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return Math.max(state - 1, 1);
+      default:
+        throw new Error(`Unhandled action type: ${action}`);
+    }
+  }, 1);
   const router = useRouter();
 
+  // const UploadArea = () => {
+  //   let array = []
+  //   for(let x = 0; x < state.numCases; x++){
+
+  //   }
+  // }
+
+  const upload = async (filePath: string, file: File | undefined) => {
+    await supabase.storage
+      .from("spec-storage")
+      .upload(filePath, file || "")
+      .catch((error) => console.log(error));
+  };
+
   const fileUpload = async () => {
-    const upload = async (filePath: string, file: File | undefined) => {
-      await supabase.storage
-        .from("spec-storage")
-        .upload(filePath, file || "")
-        .catch((error) => console.log(error));
-    };
     const inputID = nanoid();
     const outputID = nanoid();
 
@@ -150,6 +194,9 @@ const CreateAssignment: NextPage<CreateAssignmentProps> = ({ courseId }) => {
 
   return (
     <div className="flex">
+      <h1>{state.fileStore.length}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>CLICK ME</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
       <div className="text-gray-100 px-12 pt-6 flex flex-col gap-4 w-full">
         <h1 className="font-bold text-3xl">Create a new assignment</h1>
         <form className="grid grid-cols-1 gap-4">
@@ -201,7 +248,7 @@ const CreateAssignment: NextPage<CreateAssignmentProps> = ({ courseId }) => {
               </select>
             </div>
           </div>
-          <FileBox title="Input Definition"/>
+
           <div className="pb-4">
             <h1 className="text-sm font-medium text-gray-300 pb-2">Input definition</h1>
             <label
