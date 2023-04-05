85738897da62e5c75b16031ecb23ae386812ca39
e.kobedevon <e.kobedevon@gmail.com>
2023-04-05 15:51:43 -0500

    make page upload box into own component

--- pages/course/[course]/assignment/new.tsx
+++ pages/course/[course]/assignment/new.tsx
@@ -45,6 +45,48 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) =>
     };
   });
 
+
+
+const FileBox = ({ title, file }: { title: string; file?: File }) => {
+  return (
+    <div className="pb-4">
+      <h1 className="text-sm font-medium text-gray-300 pb-2">{title}</h1>
+      <label
+        htmlFor="input-def"
+        className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg aria-hidden="true" fill="none" stroke="currentColor" className="w-12 h-12 mx-auto text-gray-600">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+          </svg>
+          {!file ? (
+            <>
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span>
+              </p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">.zip (max. 50MB)</p>
+            </>
+          ) : (
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              Currently selected: <span className="font-semibold">{File?.name}</span>
+            </p>
+          )}
+        </div>
+        <input
+          id="input-def"
+          type="file"
+          className="hidden"
+          accept=".txt"
+          // onChange={(e) => setInputFile(e.target.files?.[0])}
+        />
+      </label>
+    </div>
+  );
+};
+
 const CreateAssignment: NextPage<CreateAssignmentProps> = ({ courseId }) => {
   const supabase = useSupabaseClient();
   const [inputFile, setInputFile] = useState<File | undefined>();
@@ -74,7 +116,6 @@ const CreateAssignment: NextPage<CreateAssignmentProps> = ({ courseId }) => {
   };
 
   const createRecord = async (inputPath: string, outputPath: string) => {
-    console.log("HERE: " + courseId);
     const { data: assignment, error } = await supabase
       .from("assignment")
       .insert({
@@ -161,7 +202,7 @@ const CreateAssignment: NextPage<CreateAssignmentProps> = ({ courseId }) => {
               </select>
             </div>
           </div>
-
+          <FileBox title="Input Definition"/>
           <div className="pb-4">
             <h1 className="text-sm font-medium text-gray-300 pb-2">Input definition</h1>
             <label
